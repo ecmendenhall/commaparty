43b4e8238e852bd5833a8fc6d88c00044847a1bc
Connor Mendenhall <ecmendenhall@gmail.com>
2014-03-12 14:16:23 -0500

    Start adding Hiccup specs.

--- spec/commaparty/markup_spec.rb
+++ spec/commaparty/markup_spec.rb
@@ -79,4 +79,109 @@ describe CommaParty::Markup do
     end
 
   end
+
+  describe 'Hiccup specs' do
+
+    describe 'basic tags' do
+      specify { expect(described_class.new([:div]).call).to eq("<div></div>\n") }
+      specify { expect(described_class.new(["div"]).call).to eq("<div></div>\n") }
+    end
+
+    describe 'tag syntax sugar' do
+      specify { expect(described_class.new([:'div#foo']).call).to eq("<div id=\"foo\"></div>\n") }
+      specify { expect(described_class.new([:'div.foo']).call).to eq("<div class=\"foo\"></div>\n") }
+      specify { expect(described_class.new([:'div.foo', ["bar", "baz"].join]).call).
+                to eq("<div class=\"foo\">barbaz</div>\n") }
+      specify { expect(described_class.new([:'div.a.b']).call).to eq("<div class=\"a b\"></div>\n") }
+      specify { expect(described_class.new([:'div.a.b.c']).call).to eq("<div class=\"a b c\"></div>\n") }
+      specify { expect(described_class.new([:'div#foo.bar.baz']).call).
+                to eq("<div class=\"bar baz\" id=\"foo\"></div>\n") }
+    end
+
+    describe 'tag contents' do
+      specify { expect(described_class.new([:div]).call).to eq("<div></div>\n") }
+      specify { expect(described_class.new([:h1]).call).to eq("<h1></h1>\n") }
+      specify { expect(described_class.new([:script]).call).to eq("<script></script>\n") }
+      specify { expect(described_class.new([:text]).call).to eq("<text></text>\n") }
+      specify { expect(described_class.new([:a]).call).to eq("<a></a>\n") }
+      specify { expect(described_class.new([:iframe]).call).to eq("<iframe></iframe>\n") }
+      specify { expect(described_class.new([:title]).call).to eq("<title></title>\n") }
+      specify { expect(described_class.new([:section]).call).to eq("<section></section>\n") }
+      specify { expect(described_class.new([:select]).call).to eq("<select></select>\n") }
+      specify { expect(described_class.new([:object]).call).to eq("<object></object>\n") }
+      specify { expect(described_class.new([:video]).call).to eq("<video></video>\n") }
+    end
+
+    describe 'tags containing text' do
+      specify { expect(described_class.new([:text, "Lorem Ipsum"]).call).
+                to eq("<text>Lorem Ipsum</text>\n") }
+    end
+
+    describe 'contents are concatenated' do
+      specify { expect(described_class.new([:body, "foo", "bar"]).call).
+                to eq("<body>foobar</body>\n") }
+      specify { expect(described_class.new([:body, [:p], [:br]]).call).
+                to eq("<body>\n<p></p>\n<br>\n</body>\n") }
+    end
+
+    describe 'seqs are expanded' do
+      specify { expect(described_class.new([:body, ["foo", "bar"]]).call).
+                to eq("<body>foobar</body>\n") }
+    end
+
+    describe 'tags can contain tags' do
+      specify { expect(described_class.new([:div, [:p]]).call).
+                to eq("<div><p></p></div>\n") }
+      specify { expect(described_class.new([:div, [:b]]).call).
+                to eq("<div><b></b></div>\n") }
+      specify { expect(described_class.new([:p, [:span, [:a, "foo"]]]).call).
+                to eq("<p><span><a>foo</a></span></p>\n") }
+    end
+
+    describe 'tag with blank attribute map' do
+      specify { expect(described_class.new([:xml, {}]).call).
+                to eq("<xml></xml>\n") }
+    end
+
+    describe 'tag with populated attribute map' do
+      specify { expect(described_class.new([:xml, {a: "1", b: "2"}]).call).
+                to eq("<xml a=\"1\" b=\"2\"></xml>\n") }
+      specify { expect(described_class.new([:img, {id: "foo"}]).call).
+                to eq("<img id=\"foo\">\n") }
+    end
+
+    describe 'tag content can be vars' do
+      var = "foo"
+      specify { expect(described_class.new([:span, var]).call).
+                to eq("<span>foo</span>\n") }
+    end
+
+    describe 'tag content can be function calls' do
+      specify { expect(described_class.new([:span, [1, 1].inject(:+)]).call).
+                to eq("<span>2</span>\n") }
+      specify { expect(described_class.new([:span, {foo: "bar"}.fetch(:foo)]).call).
+                to eq("<span>bar</span>\n") }
+    end
+
+    describe 'attributes can contain vars' do
+      var = "foo"
+      specify { expect(described_class.new([:xml, {x: var}]).call).
+                to eq("<xml x=\"foo\"></xml>\n") }
+      specify { expect(described_class.new([:xml, {x: var}, "bar"]).call).
+                to eq("<xml x=\"foo\">bar</xml>\n") }
+    end
+
+    describe 'attributes can contain function calls' do
+
+      def foo
+        "foo"
+      end
+
+      specify { expect(described_class.new([:img, {src: ["/foo", "/bar"].join}]).call).
+                to eq("<img src=\"/foo/bar\">\n") }
+      specify { expect(described_class.new([:div, {id: ["a", "b"].join}, foo]).call).
+                to eq("<div id=\"ab\">foo</div>\n") }
+    end
+
+  end
 end
